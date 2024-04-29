206d8872589e1c02310e2da64f81ab675bfe2dc7
Matheus Tavares <161875886+tatavaress@users.noreply.github.com>
2024-04-29 17:45:30 -0300

    Add files via upload

---
+++ tavaresapp/button(fontsize).py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.button import Button 
+
+class MinhaApp(App):
+    def build(self):
+        return Button(text='Clique Aqui', font_size=50)
+    
+if __name__ == "__main__":
+    MinhaApp().run()    
