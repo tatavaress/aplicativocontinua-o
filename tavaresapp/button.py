32600a65c6b00736282b1fa2505348a9ed12f6b6
Matheus Tavares <161875886+tatavaress@users.noreply.github.com>
2024-04-30 17:35:13 -0300

    Delete tavaresapp/button.py

--- tavaresapp/button.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.button import Button 
-
-class MinhaApp(App):
-    def build(self):
-        return Button(text='Clique Aqui')
-    
-if __name__ == "__main__":
-    MinhaApp().run()    
